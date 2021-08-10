301021bce02603908169cc89e3347b58f7421bbf
Musaib <47937241+RDJ2000@users.noreply.github.com>
2021-08-10 19:35:24 +0530

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const Pusher = require("pusher");
-
+const bodyParser = require('body-parser');
 const pusher = new Pusher({
     appId: "1248135",
     key: "6b33b615f5d8fa877cbd",
@@ -14,11 +14,24 @@ const pusher = new Pusher({
 const app = express();
 
 app.use(cors({
-    origin: ['http://localhost:3000', 'http://localhost:8080', 'http://localhost:4200','https://todo-94c45.web.app/']
+    origin: ['http://localhost:3000', 'http://localhost:8080', 'http://localhost:4200','https://todo-94c45.web.app']
+    ,"methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204
 }))
 
+const connectionParams={
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true 
+  }
+  app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.json())
-
+app.use(express.urlencoded({
+    extended: true
+  }))
 app.post('/api/messages', async (req, res) => {
     await pusher.trigger("chat", "message", {
         username: req.body.username,
